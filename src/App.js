fd1934262107b68dafe77311e8970bf3c71476b6
phmtsweb <phmtsweb@gmail.com>
2020-08-18 22:28:33 -0300

    Envio do desafio 2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("/repositories").then((response) => setRepositories(response.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("/repositories", {
+      title: `Desafio ${Date.now()}`,
+      url: `http://github.com/phmts/${Date.now()}`,
+      techs: ["Node.js", "ReactJS", "ReactNative"],
+    });
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => {
+          return (
+            <div key={repository.id}>
+              <li>{repository.title}</li>
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </div>
+          );
+        })}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <button onClick={handleAddRepository}>Adicionar</button>{" "}
     </div>
   );
 }
